380d8026a00ba58fd393ea2a066caeadc6b7347e
HaiTa <taminhtienhai@gmail.com>
2019-11-07 08:23:48 +0700

    update aws key

--- queries/dynamodb.js
+++ queries/dynamodb.js
@@ -1,15 +1,15 @@
 const AWS = require('aws-sdk')
 
 AWS.config.update({
-    accessKeyId: 'ASIA4P5MXXQW4S3XTXOY',
-    secretAccessKey: 'sqpnekZ3pm08avUIzEOYlVC4qADBoKnQKLfDP7GW',
-    sessionToken: 'FwoGZXIvYXdzEHkaDE9Q2ocjO3L/rqqx5CLGAQ1ZeTS6w9XmS9' +
-        'jxE9rVmI17IJ7FzrZDemrlW35heU5KMHOg4zJIP6zltwU2BYWkwRR/7X9o3Y' +
-        'ZbOgb7Cf85TEFjqUA0FNEOH8WwT01IedaYuUGwSlNfTRuBLKuI7PCsu3Imge1' +
-        'HbJj6I8QrlBtaHEfvBlTeO3LvDBWvS2AgDmVvoia5R/R/8e/C+97ZO5R2Si5' +
-        'SvRZLYg4iTknYih0zgs3fNB//zzYrd9wy23znu7TMCE/S1mEKrhP5IU8U96O' +
-        'CwMm1w5Te0ijar43uBTIte0IMr4ywYyFAanK0HstoFS+tTwMXFITESfUcgR8' +
-        'STDSuMw25j94bruc+eP/L',
+    accessKeyId: 'ASIA4P5MXXQW2MJK5EDR',
+    secretAccessKey: '9LWD7SMWd+LRGF/EvEemZ+zhvD/AWh5xKU6nklXK',
+    sessionToken: 'FwoGZXIvYXdzEHsaDLSl2DIupOMkqkJbtSLGAfVTjFbBVIu' +
+        'Rj5HZDZ9Sv97zBAoN7fR1rp2SDeX8m00Mxl9TREzLJQah/wycXaGPAzTKD' +
+        'XStQ9j4u9b8slGwr+oPBZ12tI/bfenNUgdPgLvG3yxXBJT6kaBkVu+2pn5' +
+        'DY3EP0u5buvLMZW2OY85HybwUwNAqpQqLLFpWaQOSP0azPkRuCqlbPZwU' +
+        'uMWASt2MNocr75sKTCbY1cyH51ZJjHekXfMpsjTGD70PYNDfPrHe9CC8N6' +
+        '88T4yjs2FwlLyI2lZ2BSIbLCjm443uBTItfbwcHiqgj345jAeVx0KnIy2' +
+        'gWC0RpnmYVpU9GiWmvdKF+KJY1boMHgigF5Vv',
     region: 'us-east-1'
 })
 
